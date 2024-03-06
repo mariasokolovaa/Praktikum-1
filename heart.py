ebc90f1b77371ccab18f8e17450af6aa42eb4ff5
Maria <mariasokolova2004@gmail.com>
2024-03-06 19:11:04 +0300

    The comments was added

--- heart.py
+++ heart.py
@@ -1,18 +1,17 @@
-# Import turtle package 
 import turtle 
 
-# Creating a turtle object(pen) 
+ 
 pen = turtle.Turtle() 
 
-# Defining a method to draw curve 
+ 
 def curve(): 
 	for i in range(200): 
 
-		# Defining step by step curve motion 
+		 
 		pen.right(1) 
 		pen.forward(1) 
 
-# Defining method to draw a full heart 
+
 def heart(): 
 
 	# Set the fill color to red 
@@ -49,7 +48,7 @@ def txt():
     pen.down() 
   
     # Set the text color to lightgreen 
-    pen.color('lightgreen') 
+    pen.color('black') 
   
     # Write the specified text in  
     # specified font style and size 
